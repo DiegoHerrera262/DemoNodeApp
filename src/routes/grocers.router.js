cd8de5bc24e06d431b1a50d0029dc007ea86b213
diegoherrera262 <diegoherrera262@gmail.com>
2021-07-21 08:53:56 -0500

    feature: minor changes.

--- src/routes/grocers.router.js
+++ src/routes/grocers.router.js
@@ -5,13 +5,11 @@ const upload = multer();
 
 const grocersController = require("../controllers/grocers.controller");
 
-
 // route for creating grocer from web app
 router.post("/grocer/create", upload.any(), grocersController.grocerCreate);
 router.get("/grocers", grocersController.grocerGet);
 router.get("/grocer/:id", grocersController.grocerGetById);
-router.put("/grocer/:id", grocersController.grocerUpdate);
+router.put("/grocer/:id", upload.any(), grocersController.grocerUpdate);
 router.delete("/grocer/:id", grocersController.grocerDelete);
-
 
 module.exports = router;
